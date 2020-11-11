de5a9ba276e57a5aa706ada1077bcda66d9a1e2d
guanxun <gx_steven@163.com>
2020-11-11 16:32:25 +0800

    冒泡排序优化算法

--- src/main/java/com/guanxun/sort/BubbleSort.java
+++ src/main/java/com/guanxun/sort/BubbleSort.java
@@ -10,14 +10,17 @@ public class BubbleSort {
 
     public static void main(String[] args){
 
-        int[] array = new int[]{5,8,6,3,9,2,1,7};
-        sort1(array);
-        System.out.println(Arrays.toString(array));
+        int[] array1 = new int[]{5,8,6,3,9,2,1,7};
+        sort1(array1);
+        System.out.println(Arrays.toString(array1));
 
+        int[] array2 = new int[]{5,8,6,3,9,2,1,7};
+        sort2(array2);
+        System.out.println(Arrays.toString(array2));
     }
 
     /**
-     * 普通冒泡排序（pull测试一下）
+     * 普通冒泡排序（从小到大）
      * */
     private static void sort1(int[] array){
 
@@ -30,7 +33,32 @@ public class BubbleSort {
                 }
             }
         }
-
     }
+
+    /**
+     * 冒泡排序优化算法
+     * 当判断到后续没有进行元素交换，说明已经排序完成了。
+     * */
+    public static void sort2(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++){
+            // 有序标记，每一轮初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    //因为有元素进行交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                break;
+            }
+        }
+    }
+
+
 
 }
