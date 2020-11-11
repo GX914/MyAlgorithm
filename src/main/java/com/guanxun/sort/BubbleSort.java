7905f74a25e2e32b05028ea78796e6f2df2e4a7f
guanxun <gx_steven@163.com>
2020-11-11 18:12:37 +0800

    鸡尾酒算法

--- src/main/java/com/guanxun/sort/BubbleSort.java
+++ src/main/java/com/guanxun/sort/BubbleSort.java
@@ -21,6 +21,10 @@ public class BubbleSort {
         int[] array3 = new int[]{5,8,6,3,9,2,1,7};
         sort3(array3);
         System.out.println(Arrays.toString(array3));
+
+        int[] array4 = new int[]{5,8,6,3,9,2,1,7};
+        sort4(array4);
+        System.out.println(Arrays.toString(array4));
     }
 
     /**
@@ -87,6 +91,45 @@ public class BubbleSort {
                 }
             }
             sortBorder = lastExchangeIndex;
+            if (isSorted){
+                break;
+            }
+        }
+    }
+
+    /**
+     * 鸡尾酒排序算法
+     * */
+    public static void sort4(int[] array){
+
+        int tmp = 0;
+        for (int i = 0; i < array.length/2; i++){
+            // 有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            // 奇数轮，从左向右比较和交换
+            for (int j = 1; j < array.length - i - 1; j++){
+                if (array[j] > array[j+1]){
+                    tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                break;
+            }
+            // 在偶数轮之前，将isSorted重新标记为true
+            isSorted = true;
+            // 偶数轮,从右向左比较和交换
+            for (int j = array.length - i - 1; j > i; j--){
+                if(array[j] < array[j-1]){
+                    tmp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = tmp;
+                    // 因为有元素进行交换，所以不是有序的，标记变为false
+                    isSorted = false;
+                }
+            }
             if (isSorted){
                 break;
             }
